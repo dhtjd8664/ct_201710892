dfdf34a88a02ba883fe0e2ee6a2bd9812dbba499
dhtjd8664 <dhtjd8664@naver.com>
2017-04-17 15:42:05 +0900

    Add files via upload

---
+++ move turtle(my face).py
@@ -0,0 +1,34 @@
+Python 2.7.6 (default, Nov 10 2013, 19:24:24) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> wn=turtle.Screen()
+
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    wn=turtle.Screen()
+NameError: name 'turtle' is not defined
+>>> import turtle
+>>> wn=turtle.Screen()
+>>> t1 = turtle.Turtle()
+>>> wn.addshape("C:\\Users\\400T6B\\Documents\\rocketship.gif")
+>>> t1.shape("C:\\Users\\400T6B\\Documents\\rocketship.gif")
+>>> ================================ RESTART ================================
+>>> import turtle
+>>> wn=turtle.Screen()
+>>> t1=turtle.Turtle()
+\
+>>> wn.addshape("C:\\Users\\400T6B\\Documents\\dragon.gif")
+>>> t1.shape("C:\\Users\\400T6B\\Documents\\dragon.gif")
+>>> ================================ RESTART ================================
+>>> import turtle
+>>> wn=turtle.Screen()
+>>> t1 = turtle.Turtle()
+>>> wn.addshape("C:\\Users\\400T6B\\Documents\\Kwon o seong.gif")
+>>> t1.shape("C:\\Users\\400T6B\\Documents\\Kwon o seong.gif")
+>>> t1.fd(100)
+>>> t1.rt(90)
+>>> t1.fd(100)
+>>> t1.rt(90)
+>>> t1.fd(100)
+>>> t1.rt(90)
+>>> t1.fd(100)
+>>> 
