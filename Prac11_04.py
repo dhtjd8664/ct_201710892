0c51e1a763a73ea4e9ed39cc03c0b7714916d75d
dhtjd8664 <dhtjd8664@naver.com>
2017-05-21 22:25:39 +0900

    Add files via upload

---
+++ Prac11_04.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[1]:
+
+from PIL import Image
+
+
+# In[2]:
+
+m = Image.open("C:\\Users\\Public\\Pictures\\italy-1587287_1280.jpg")
+
+
+# In[3]:
+
+m.save("C:\\Users\\Public\\Pictures\\italy-1587287_1280.gif")
+
